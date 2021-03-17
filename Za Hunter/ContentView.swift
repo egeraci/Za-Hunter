0d4261fbbb7c74fda46ab46fb4298a0e20ff812f
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-17 16:01:11 -0500

    Made Progress!

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+    @State private var places = [Place]()
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode:MapUserTrackingMode = .follow
     @State private var regan = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 42.15704, longitude: -88.14812), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
@@ -16,7 +17,34 @@ struct ContentView: View {
     
     
     var body: some View {
-        Map(coordinateRegion: $regan, interactionModes: .all, userTrackingMode: $userTrackingMode)
+        Map(coordinateRegion: $regan, interactionModes: .all, showsUserLocation: true, userTrackingMode: $userTrackingMode, annotationItems: places)
+            {
+            place in MapPin(coordinate: place.a)
+                {
+                    
+                }
+            })
+        
+    }
+    
+    func performsearch(item: String)
+    {
+        let searchReaquest = MKLocalSearch.Request()
+        searchReaquest.naturalLanguageQuery = item
+        searchReaquest.region = regan
+        let search = MKLocalSearch(request: searchReaquest)
+        search.start { (response, Error) in
+            if let response = response
+            {
+                for mapitem in response.mapItems
+                {
+                    let anidation = MKPointAnnotation()
+                    anidation.coordinate = mapitem.placemark.coordinate
+                    anidation.title = mapitem.name
+                    places.append(Place(anadation: anidation, mapidem: mapitem))
+                }
+            }
+        }
     }
 }
 
@@ -26,3 +54,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct Place: Identifiable
+{
+    let id = UUID()
+    let anadation: MKPointAnnotation
+    let mapidem: MKMapItem
+}
